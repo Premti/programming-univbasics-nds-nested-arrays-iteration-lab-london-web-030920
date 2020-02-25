fda4e4e2894162c74c39d15dbfced79f46942254
premti <premti@gmail.com>
2020-02-25 13:49:45 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -26,8 +26,9 @@ def find_greater_pair(src)
       new_array.push(src[element_index][0])
     else 
       new_array.push(src[element_index][1])
-      element_index += 1
   end
+  element_index += 1 
+end
   return new_array
 end
 
